23aef059374ffe07c667f4f5c422c94cf3d36db7
Rodzelivans <rodzelivans@gmail.com>
2026-07-25 10:09:08 +0800

    Create data_cleaning.sql

---
+++ script/silver/data_cleaning.sql
@@ -0,0 +1,46 @@
+INSERT INTO silver.crm_cust_info(cst_id, cst_key, cst_first_name, cst_last_name, cst_material_status, cst_gndr, cst_create_date)
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_first_name) AS cst_first_name,
+TRIM(cst_last_name) AS cst_last_name,
+CASE WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
+	 WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
+	 ELSE 'n/a'
+END cst_material_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	 ELSE 'n/a'
+END cst_gndr,
+cst_create_date
+FROM (
+	SELECT *, ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS Ordered_list
+	FROM "Bronze".crm_cust_info)
+WHERE ordered_list = 1;
+
+
+INSERT INTO silver.crm_prd_info(
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt
+)
+SELECT prd_id,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key,
+prd_nm,
+COALESCE(prd_cost, 0) AS prd_cost,
+CASE WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+	 WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+	 WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+	 WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+	 ELSE 'n/a'
+END AS prd_line,
+prd_start_dt,
+LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt
+FROM "Bronze".crm_prd_info;
+

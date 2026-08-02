5a54a26852df860ecff77af8c8f38cc1cffb0ed9
Rodzelivans <rodzelivans@gmail.com>
2026-08-02 22:13:36 +0800

    Update dimensions.sql

--- script/Gold/dimensions.sql
+++ script/Gold/dimensions.sql
@@ -18,3 +18,20 @@ ON ci.cst_key = ca.cid
 INNER JOIN silver.erp_loc loc
 ON ci.cst_key = loc.cid
 
+CREATE VIEW gold.dim_products AS
+SELECT
+ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+pn.prd_id AS product_id,
+pn.prd_key AS product_number,
+pn.prd_nm AS product_name,
+pn.cat_id AS category_id,
+pc.cat AS category,
+pc.subcat AS subcategory,
+pc.maintenance,
+pn.prd_cost AS cost,
+pn.prd_line AS product_line,
+pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat pc
+ON pn.cat_id = pc.px_id
+WHERE prd_end_dt IS NULL
